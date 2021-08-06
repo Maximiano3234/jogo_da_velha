2b71a19604a2b27ab385afe5c8c1a0d6c21cedc0
Maximiano S. Ramiro <maximusterceiro3@hotmail.com>
2021-08-06 20:09:13 -0300

    funções

---
+++ script.js
@@ -0,0 +1,125 @@
+// Inicial Data
+let square = {
+    a1: '', a2: '', a3: '',
+    b1: '', b2: '', b3: '',
+    c1: '', c2: '', c3: ''
+};
+let player = '';
+let warning = '';
+let playing = false;
+
+reset();
+// Events
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick);
+});
+
+
+// Functions
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    if(playing && square[item] === '') {
+        square[item] = player;
+        renderSquere();
+        togglePlayer();
+    }
+}
+
+function reset(){
+    warning = '';
+
+    let random = Math.floor(Math.random() * 2);
+    player = (random === 0) ? 'x' : 'o';
+
+    for(let i in square) {
+        square[i] = '';
+    }
+
+    playing = true;
+
+    renderSquere();
+    renderInfo();
+}
+ function renderSquere() {
+     for(let i in square) {
+         let item = document.querySelector(`div[data-item=${i}]`);
+         if(square[i] !== '') {
+             item.innerHTML = square[i];
+         } else {
+             item.innerHTML = '';
+         }
+     }
+
+     checkGame();
+
+ }
+
+ function renderInfo() {
+     document.querySelector('.vez').innerHTML = player;
+     document.querySelector('.resultado').innerHTML = warning;
+
+ }
+ function togglePlayer() {
+    player = (player === 'x') ? 'o' : 'x';
+    renderInfo(); 
+    
+    
+    /*if(player === 'x') {
+         player = 'o';
+     } else {
+         player = 'x';
+     }*/
+ }
+
+ function checkGame() {
+     if(checkWinnerFor('x')) {
+         warning = 'O "x" venceu';
+         playing = false;
+     } else if(checkWinnerFor('o')) {
+         warning = 'O "o" venceu';
+         playing = false;
+     } else if(isFull()) {
+         warning = 'Deu empate';
+         playing = false;
+     }
+ }
+
+ function checkWinnerFor(Player) {
+        let pos = [
+            'a1,a2,a3',
+            'b1,b2,b3',
+            'c1,c2,c3',
+
+            'a1,b1,c1',
+            'a2,b2,c2',
+            'a3,b3,c3',
+
+            'a1,b2,c3',
+            'a3,b2,c1',
+
+        ];
+        for(let w in pos) {
+            let pArray = pos[w].split(','); // array possibilit
+            let hasWon = pArray.every(option => square[option] === player);
+            if(hasWon) { 
+                return true;
+            /*pArray.every((option)=>{
+                if(square[option] === player) {
+                    return true;
+                } else {
+                    return false;
+                }                
+            });*/
+        }
+    }
+    return false;
+ }
+ function isFull() {
+    for(let i in square) {
+        if(square[i] === '') {
+            return false;
+        }
+    }
+    return true;
+ }
